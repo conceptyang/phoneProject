9da6cd624afc419138d359aad191b44b0d76eb07
conceptyang <489397074@qq.com>
2016-12-09 20:37:47 +0800

    change index.js to add some click event

--- tcEj/js/index.js
+++ tcEj/js/index.js
@@ -1,1 +1,3 @@
-$("#footer").load("pagefooter.html",function(){new Swiper(".swiper-container",{loop:!0,pagination:".swiper-pagination",paginationClickable:!0,autoplay:2e3})});
+$("#footer").load("pagefooter.html",function(){new Swiper(".swiper-container",{loop:!0,pagination:".swiper-pagination",paginationClickable:!0,autoplay:2e3});
+    $(".paging").click(function(){$(this).siblings().css("color","#a1a1a2"),$(this).css("color","skyblue"),console.log($(this))})
+});
